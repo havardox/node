58a3e67e3649f2624adca1856f2adf319b1b1669
Harald Varner <harald.varner@gmail.com>
2020-07-24 15:49:09 +0300

    variable port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page baby");
 });
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Wazzappppp");
 });
